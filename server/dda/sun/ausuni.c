f1082a9c77a2ad732c7776065b8a487faa2b24be
Jon Trulson <jon@radscan.com>
2000-09-04 18:04:17 +0000

    add Id tag

--- server/dda/sun/ausuni.c
+++ server/dda/sun/ausuni.c
@@ -26,6 +26,8 @@ not be used in advertising or otherwise to promote the sale, use or
 other dealings in this Software without prior written authorization
 from the copyright holder.
 
+ $Id$
+
 */
 
 /**
