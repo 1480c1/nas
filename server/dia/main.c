f1e8e0da221152560efcb097c00539476071047c
Petr Písař <ppisar@redhat.com>
2020-01-23 13:43:12 +0100

    Fix building with GCC 10
    GCC 10 defaults to -fno-common and as a result raises an arror when
    linking nasd:
    
    gcc -o nasd -O2 -fno-strict-aliasing   -Wl,-z,relro -Wl,--as-needed  -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld  -L../lib/audio -L/usr/lib64 -L/usr/lib64    dia/libdia.a dda/voxware/libvoxware.a os/libos.a
    /usr/bin/ld: dia/libdia.a(lex.o): in function `$d':
    lex.c:(.bss+0x48): multiple definition of `yyin'; dia/libdia.a(main.o):/builddir/build/BUILD/nas-1.9.4/server/dia/main.c:79: first defined here
    collect2: error: ld returned 1 exit status
    
    The reason is that both lex.c (generated from lex.l) and main.c
    define yyin global variable.
    
    This patch changes the main.c definition into a declaration.
    
    Signed-off-by: Petr Písař <ppisar@redhat.com>

--- server/dia/main.c
+++ server/dia/main.c
@@ -76,7 +76,7 @@ static char *AuServerName(void);
 extern char *display;
 
 static int restart = 0;
-FILE *yyin;                     /* for the config parser */
+extern FILE *yyin;                     /* for the config parser */
 
 void
 NotImplemented()
